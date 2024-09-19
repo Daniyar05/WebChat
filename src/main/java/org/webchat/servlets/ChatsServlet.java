c2d1a1c2162b9a932611cd4c7e7fcf7ba68714bd
Daniyar05 <abdullin1234321zxc@gmail.com>
2024-09-19 13:49:22 +0300

    Create Main Files and Add html/css files

---
+++ src/main/java/org/webchat/servlets/ChatsServlet.java
@@ -0,0 +1,10 @@
+package org.webchat.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "ChatsServlet", value = "/chats")
+
+public class ChatsServlet extends HttpServlet {
+
+}
